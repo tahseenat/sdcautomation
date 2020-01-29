43e2a021aa18f769c6bcd80f8a21e3ec7b7fc09e
Tahseen Anwar Tahir <37225838+tahseenat@users.noreply.github.com>
2020-01-29 22:11:18 +0530

    navigation to contacts added in scrolli.py and sdcautomation.py now only traverse the links.

---
+++ scratch.py
@@ -0,0 +1,5 @@
+import  time
+time.sleep(5)
+message = "Hello, \bsexies\b!!! We just sent you the invitation to the luxury erotic weekend Barcelona S.Fantasy which will take place on the last weekend of June! :) We'd love you to attend it and the location is a gorgeous 15 rooms villa just West of the city (if you'd like to be accommodated in the villa, let us know asap). The event is limited to people up to 50 years of age. \n For more info, copy or type this link into your browser: event.sexy \n or contact us by Whatsapp +1.516.577.0507. \n Kisses, Margarita and Luca 😘"
+import keyboard
+keyboard.write(message)
