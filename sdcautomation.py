819c5a66e0835991057d22402178623a2daec99c
Tahseen Anwar Tahir <37225838+tahseenat@users.noreply.github.com>
2020-02-01 20:31:50 +0530

    final commits

--- sdcautomation.py
+++ sdcautomation.py
@@ -18,37 +18,43 @@ def login():
 
 
 def invite():
-    time.sleep(soft_wait)
-    driver.find_element(By.XPATH, "//*[contains(text(), 'Invite')]").click()
-    time.sleep(soft_wait)
-    party_xpath = "//*[contains(text(), '{}')]".format(name_of_party)
-    driver.find_element(By.XPATH, party_xpath).click()
-    time.sleep(soft_wait)
-    driver.find_element(By.XPATH, "//*[contains(text(), 'Cancel')]").click()
-    time.sleep(soft_wait)
+    try:
+        time.sleep(soft_wait)
+        driver.find_element(By.XPATH, "//*[contains(text(), 'Invite')]").click()
+        time.sleep(soft_wait)
+        party_xpath = "//*[contains(text(), '{}')]".format(name_of_party)
+        driver.find_element(By.XPATH, party_xpath).click()
+        time.sleep(soft_wait)
+        driver.find_element(By.XPATH, "//*[contains(text(), 'Cancel')]").click()
+        time.sleep(soft_wait)
+    except:
+        pass
 
 
 def mail():
-    time.sleep(soft_wait)
-    driver.find_element(By.XPATH, "//*[contains(text(), 'Email')]").click()
-    time.sleep(long_wait)
-    driver.find_element(By.XPATH, '//*[@class="ql-editor ql-blank"]').click()
-    time.sleep(soft_wait)
-    keyboard.write(message)
-    time.sleep(soft_wait)
+    try:
+        time.sleep(soft_wait)
+        driver.find_element(By.XPATH, "//*[contains(text(), 'Email')]").click()
+        time.sleep(long_wait)
+        driver.find_element(By.XPATH, '//*[@class="ql-editor ql-blank"]').click()
+        time.sleep(soft_wait)
+        keyboard.write(message)
+        time.sleep(soft_wait)
 
-    # click on Add photo
-    time.sleep(soft_wait)
-    driver.find_element(By.XPATH, "//*[contains(text(), 'Add Photo')]").click()
-    time.sleep(soft_wait)
+        # click on Add photo
+        time.sleep(soft_wait)
+        driver.find_element(By.XPATH, "//*[contains(text(), 'Add Photo')]").click()
+        time.sleep(soft_wait)
 
-    # enter the location of the image file you want send in next line
-    autoit.control_set_text("Open", "Edit1", r"D:\ME\c dfrive\New folder\557421.jpg")
-    autoit.control_send("Open", "Edit1", "{ENTER}")
-    time.sleep(soft_wait)
+        # enter the location of the image file you want send in next line
+        autoit.control_set_text("Open", "Edit1", r"D:\ME\c dfrive\New folder\557421.jpg")
+        autoit.control_send("Open", "Edit1", "{ENTER}")
+        time.sleep(soft_wait)
 
-    driver.find_element(By.XPATH, "//*[contains(text(), 'Send')]").click()
-    time.sleep(long_wait + soft_wait + long_wait)
+        driver.find_element(By.XPATH, "//*[contains(text(), 'Send')]").click()
+        time.sleep(long_wait + soft_wait + long_wait)
+    except:
+        pass
 
 
 if __name__ == "__main__":
@@ -96,3 +102,4 @@ if __name__ == "__main__":
         time.sleep(soft_wait)
         invite()
         driver.close()
+    driver.close()
