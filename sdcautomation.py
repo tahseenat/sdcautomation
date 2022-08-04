d349f2b5483e75a84c890538b588a7571c20e57c
Tahseen Anwar Tahir <37225838+tahseenat@users.noreply.github.com>
2022-08-04 17:05:12 +0530

    Updated code for new messenger flow

--- sdcautomation.py
+++ sdcautomation.py
@@ -31,11 +31,36 @@ def invite():
         time.sleep(soft_wait)
         driver.find_element(By.XPATH, "//*[contains(text(), 'Invite')]").click()
         time.sleep(soft_wait)
-        party_xpath = "//*[contains(text(), '{}')]".format("Tuscany")
+        party_xpath = "//*[contains(text(), '{}')]".format("Margarita")
         driver.find_element(By.XPATH, party_xpath).click()
         time.sleep(soft_wait)
         driver.find_element(By.XPATH, "//*[contains(text(), 'Cancel')]").click()
         time.sleep(soft_wait)
+    except:
+        pass
+
+
+def mail2():
+    try:
+        time.sleep(soft_wait)
+        driver.find_element(By.XPATH, "(//*[contains(text(), 'Messenger')])[2]").click()
+        time.sleep(long_wait)
+        driver.find_element(By.XPATH, '//*[@id="input_message"]').click()
+        time.sleep(soft_wait)
+        keyboard.write(message)
+        time.sleep(soft_wait)
+        # click on Add photo
+        time.sleep(soft_wait)
+        driver.find_element(By.XPATH, """(//*[@class="cursor-pointer" and @role = "presentation"])[2]""").click()
+        time.sleep(soft_wait)
+        driver.find_element(By.XPATH, """//*[@class="cursor-pointer" and contains(text(),'Photos')]""").click()
+        time.sleep(soft_wait)
+        # enter the location of the image file you want send in next line
+        autoit.control_set_text("Open", "Edit1", r"D:\ME\c dfrive\New folder\557421.jpg")
+        autoit.control_send("Open", "Edit1", "{ENTER}")
+        time.sleep(soft_wait)
+        driver.find_element(By.XPATH, """(//*[@class="cursor-pointer" and @role = "presentation"])[1]""").click()
+        time.sleep(long_wait)
     except:
         pass
 
@@ -64,51 +89,51 @@ def mail():
 
 
 if __name__ == "__main__":
-    # enter credentials
-    username = "MASTERANDMARGARITA"
-    password = "veloce26."
-    website_URL = "https://www.sdc.com/"
+# enter credentials
+username = "masterandmargarita7788@gmail.com"
+password = "SDCaccess"
+website_URL = "https://www.sdc.com/"
 
-    # enter content to send
-    message = "Hello, sexies!!!"
+# enter content to send
+message = "Hello, sexies!!!"
 
-    name_of_party = "Saint Valentine Party | Friday, February 14, 2020"
+name_of_party = "Saint Valentine Party | Friday, February 14, 2020"
 
-    # specify waits in sec
-    soft_wait = 2
-    long_wait = 8
+# specify waits in sec
+soft_wait = 2
+long_wait = 8
 
-    # load database
-    file_name = 'Database.csv'
-    csvFile = open(file_name, 'r', newline='')
-    csvWriter = csv.writer(csvFile)
+# load database
+file_name = 'Database.csv'
+csvFile = open(file_name, 'r', newline='')
+csvWriter = csv.writer(csvFile)
 
-    # reading file in pandas DataFrame
-    user_id = pd.read_csv(file_name, encoding="ISO-8859-1", usecols=range(0, 2))
+# reading file in pandas DataFrame
+user_id = pd.read_csv(file_name, encoding="ISO-8859-1", usecols=range(0, 2))
 
-    # driver = webdriver.Chrome(executable_path='D:/Downloads/chromedriver_win32/chromedriver.exe')
-    driver = webdriver.Chrome()
-    driver.get(website_URL)
-    time.sleep(long_wait)
+# driver = webdriver.Chrome(executable_path='D:/Downloads/chromedriver_win32/chromedriver.exe')
+driver = webdriver.Chrome()
+driver.get(website_URL)
+time.sleep(long_wait)
 
-    # login
-    login()
+# login
+login()
 
-    start_sending_from_user = 0
-    for index in tqdm(range(start_sending_from_user, len(user_id))):
-        # open tab
-        if user_id.at[index, 'Flag'] == "Not Sent":
-            driver.switch_to.window((driver.window_handles[0]))
-            driver.execute_script("window.open('');")
-            driver.switch_to.window(driver.window_handles[1])
-            time.sleep(1)
-            id_url = "https://www.sdc.com/react/#/profile?idUser={}".format(user_id.at[index, 'User_id'])
-            driver.get(id_url)
-            time.sleep(long_wait)
-            mail()
-            time.sleep(soft_wait)
-            invite()
-            driver.close()
-            user_id.at[index, 'Flag'] = "Sent"
-            user_id.to_csv(file_name, index=False)
-    driver.close()
+start_sending_from_user = 0
+for index in tqdm(range(start_sending_from_user, len(user_id))):
+    # open tab
+    if user_id.at[index, 'Flag'] == "Not Sent":
+        driver.switch_to.window((driver.window_handles[0]))
+        driver.execute_script("window.open('');")
+        driver.switch_to.window(driver.window_handles[1])
+        time.sleep(1)
+        id_url = "https://www.sdc.com/react/#/profile?idUser={}".format(user_id.at[index, 'User_id'])
+        driver.get(id_url)
+        time.sleep(long_wait)
+        invite()
+        time.sleep(soft_wait)
+        mail2()
+        driver.close()
+        user_id.at[index, 'Flag'] = "Sent"
+        user_id.to_csv(file_name, index=False)
+driver.close()
